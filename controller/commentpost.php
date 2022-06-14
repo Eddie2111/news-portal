8a1ac2dd1e94140364d244526408ae486715788d
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-06-15 00:45:34 +0600

    final push

--- controller/commentpost.php
+++ controller/commentpost.php
@@ -5,7 +5,7 @@ $comment = $_POST['comment'];
 echo $id, $comment;
 include_once '../model/dbconfig.php';
 
-    $username = 'khadim!';
+    $username = 'user.name';
     $date = date("Y-m-d");
     $sql = "INSERT INTO `comments` (`id`, `username`, `comment`, `date`, `article_id`) VALUES (NULL, '$username', '$comment', '$date', '$id'); ";
             #"INSERT INTO `comments` (`id`, `username`, `comment`, `date`, `article_id`) VALUES (NULL, 'khadim!', 'blyat!', '2022-06-01', '85') "
